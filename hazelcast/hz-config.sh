ab6437a72bd6bc3f454002f22c83fbcb76c23251
pfordahl <pfordahl78@gmail.com>
2015-12-09 11:26:33 -0600

    updated hz config script

--- hazelcast/hz-config.sh
+++ hazelcast/hz-config.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+hzxml=/opt/hazelcast/hazel*/bin/hazelcast.xml
 stackname=`uname -a | awk {'print $2'} | awk -F "-" {'print $1'}`
-rm hzservers
 counter=1
+errorcount=1
 while true
 do
 host="$stackname-hz$counter"
@@ -14,9 +15,16 @@ ecode=`echo $?`
                 echo $host >>hzservers
                 ((counter++))
                 #$counter=$(expr $counter + 1)
-        else
-                echo "no more servers"
+        elif
+                [ "$errorcount" = 10 ]
+        then
                 break
+                echo "no more servers"
+        else
+                ((counter++))
+                ((errorcount++))
+                echo "errorcount:"$errorcount
+
         fi
 done
 servers=`cat hzservers`
@@ -25,14 +33,8 @@ for i in $servers
 do
 echo "server:" $i
 echo "running awk"
-awk '{print} /<member-list>/{ print substr($0,1,match($0,/[^[:space:]]/)-1) "<member>'$i'</member>" }' hazelcast.xml >>hazelcast.xml.new
+sed -i 's/localhost/'$stackname-mc1'/g' $hzxml
+sed -i '/<member-list>/a  \                      \<member>'$i'</member>' $hzxml
 echo "done running aws"
 done
-
-#echo "exitcode:"$ecode
-
-#if
-#for i in $array
-#do
-#       if
-#               [ $i
+rm hzservers
